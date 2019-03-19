11b174fa03244cc72f61dd47e00af4da42867cca
amiraghazali <48274939+amiraghazali@users.noreply.github.com>
2019-03-19 23:13:05 +0800

    Update client.c

--- client.c
+++ client.c
@@ -12,7 +12,7 @@ int main(int argc, char const *argv[])
     struct sockaddr_in address; 
     int sock = 0, valread; 
     struct sockaddr_in serv_addr; 
-    char *hello = "Hello from client"; 
+    char *wannable = "wannable from client"; 
     char buffer[1024] = {0}; 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     { 
@@ -37,7 +37,7 @@ int main(int argc, char const *argv[])
         printf("\nConnection Failed \n"); 
         return -1; 
     } 
-    send(sock , hello , strlen(hello) , 0 ); 
+    send(sock , wannable , strlen(wannable) , 0 ); 
     printf("WANNABLE\n"); 
     valread = read( sock , buffer, 1024); 
     printf("%s\n",buffer ); 
